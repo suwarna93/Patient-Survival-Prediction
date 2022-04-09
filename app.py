43f46278d7cdcf819510f17e1a815f7fd778344f
suwarna baraskar <78205912+suwarna93@users.noreply.github.com>
2022-04-09 12:13:30 +0530

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 from keras.models import load_model
+from tensorflow import keras
 import tensorflow as tf
 
 from prediction import get_prediction
