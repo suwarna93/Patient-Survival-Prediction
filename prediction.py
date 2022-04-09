c1eca564e16bc06777c68ff3992ad41246988326
suwarna baraskar <78205912+suwarna93@users.noreply.github.com>
2022-04-09 11:52:54 +0530

    first

---
+++ prediction.py
@@ -0,0 +1,16 @@
+import numpy as np
+import tensorflow as tf 
+import keras
+from keras.models import load_model
+
+
+
+model = load_model('keras_model.h5')
+
+
+
+def get_prediction(data,model):
+    """
+    Predict the class of a given data point.
+    """
+    return model.predict(data)
